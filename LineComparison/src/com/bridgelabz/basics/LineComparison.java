971ce701448271911e3a85c5f9e0e981ae91a456
Apex4598 <login2ajit@gmail.com>
2022-05-19 00:00:25 +0530

    Added Fuctionality to Check Equality

--- LineComparison/src/com/bridgelabz/basics/LineComparison.java
+++ LineComparison/src/com/bridgelabz/basics/LineComparison.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class LineComparison {
 
-    public double x1, x2, y1, y2, length1;
+    public double x1, x2, y1, y2,x3,x4,y3,y4,length1,length2;
     Scanner sc=new Scanner(System.in);
     public void calculateLength()
     {
@@ -24,9 +24,29 @@ public class LineComparison {
         double length1=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
         System.out.println("Length of line is :" +length1);
     }
+    public void checkEquality() {
+        System.out.println("Enter the value of x3 : ");
+        x3 = sc.nextInt();
+        System.out.println("Enter the value of y3 : ");
+        y3 = sc.nextInt();
+        System.out.println("Enter the value of x4: ");
+        x4 = sc.nextInt();
+        System.out.println("Enter the value of y4 : ");
+        y4 = sc.nextInt();
+
+        double length2 = Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+
+        System.out.println("Length of line is :" + length2);
+        if (length1==length2) {
+            System.out.println("Two Lines are Equal");
+        } else {
+            System.out.println("Two Lines are Not Equal");
+        }
+    }
     public static void main(String[] args)
     {
         LineComparison lineComparison = new LineComparison();
         lineComparison.calculateLength();
+        lineComparison.checkEquality();
     }
 }
